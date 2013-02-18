9c6e75a5dfd74331623ae01a7e8ff1fa74565121
smithmx <smithmx@gmail.com>
2013-02-18 09:38:04 +0100

    Get chirps to display

--- Source/Chirp.Web/Features/stream/viewModel.js
+++ Source/Chirp.Web/Features/stream/viewModel.js
@@ -4,16 +4,14 @@
         //Bifrost.features.featureManager.get("stream").defineViewModel(function () {
             var self = this;
             var session = sessionManager;
-
-            this.chirps = readingStreamForReader.all();
+            
+            readingStreamForReader.readerId(session.getCurrentUserId());
 
             this.loadChirps = function () {
                 //$.get("/Streamer/GetReadingStreamFor", { reader: session.getCurrentUserId() }, function (e) {
                 //    self.chirps(e);
                 //});
                 //var stream = readingStream.byReader(session.getCurrentUserId());
-
-                readingStreamForReader.readerId(session.getCurrentUserId());
                 readingStreamForReader.execute();
             }
 
@@ -21,9 +19,16 @@
                 self.loadChirps();
             });
 
-
-            this.loadChirps();
-        })
+            this.streams = readingStreamForReader.all();
+        //this.loadChirps();
+            this.chirps = ko.computed(function () {
+                if (self.streams().length) {
+                    var theChirps = self.streams()[0].content;
+                    return theChirps;
+                }
+                return [];
+            });
+    })
 });
 
 Bifrost.features.featureManager.get("stream").defineViewModel(Chirp.streamFeature);
