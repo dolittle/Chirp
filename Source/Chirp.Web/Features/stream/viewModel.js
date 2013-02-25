cc54354255a1e013cc439611573cb3ed7cb1d7c4
smithmx@gmail.com <smithmx@gmail.com>
2013-02-25 19:22:51 +0100

    Remove commented out code

--- Source/Chirp.Web/Features/stream/viewModel.js
+++ Source/Chirp.Web/Features/stream/viewModel.js
@@ -1,26 +1,21 @@
 ﻿Bifrost.namespace("Chirp", {
     streamFeature: Bifrost.Type.extend(function (readingStreamForReader, sessionManager) {
 
-        //Bifrost.features.featureManager.get("stream").defineViewModel(function () {
             var self = this;
             var session = sessionManager;
             
             readingStreamForReader.readerId(session.getCurrentUserId());
 
             this.loadChirps = function () {
-                //$.get("/Streamer/GetReadingStreamFor", { reader: session.getCurrentUserId() }, function (e) {
-                //    self.chirps(e);
-                //});
-                //var stream = readingStream.byReader(session.getCurrentUserId());
                 readingStreamForReader.execute();
-            }
+            };
 
             $.subscribe("reload", function () {
                 self.loadChirps();
             });
 
             this.streams = readingStreamForReader.all();
-        //this.loadChirps();
+
             this.chirps = ko.computed(function () {
                 if (self.streams().length) {
                     var theChirps = self.streams()[0].content;
