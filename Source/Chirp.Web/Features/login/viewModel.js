99f7b970bcf829dd4c64a9f6b26e37afab66fb61
smithmx@gmail.com <smithmx@gmail.com>
2013-02-25 19:50:27 +0100

    Remove commented out code

--- Source/Chirp.Web/Features/login/viewModel.js
+++ Source/Chirp.Web/Features/login/viewModel.js
@@ -25,79 +25,7 @@
                 });
             }, 2000);
         };
-
-        //this.loginCommand = Bifrost.commands.Command.create({
-        //    options: {
-        //        name: "Login",
-        //        context: self,
-        //        properties: {
-        //            userName: ko.observable(""),
-        //            password: ko.observable("")
-        //        },
-        //        beforeExecute: clearMessages,
-        //        success: function () {
-        //            session.getUserIdFor(self.loginCommand.userName()).continueWith(function (userId) {
-        //                session.setSessionId(userId);
-        //                History.pushState({}, "", "/home");
-        //            });
-        //        },
-        //        error: function (e) {
-        //            if (e.validationResults.length) {
-        //                self.message(e.validationResults[0].errorMesssage);
-        //            }
-        //            if (e.commandValidationMessages && e.commandValidationMessages.length) {
-        //                self.message(e.commandValidationMessages[0]);
-        //            }
-        //        }
-        //    }
-        //});
-
     })
 });
 
-
-//(function (undefined) {
 Bifrost.features.featureManager.get("login").defineViewModel(Chirp.loginFeature);
-//        //function () {
-//        //var self = this;
-//        //var session = Bifrost.dependencyResolver.resolve(Chirp, "Session");
-
-//        //console.log(session);
-
-//        //function clearMessages() {
-//        //    self.message("");
-//        //}
-
-//        //this.message = ko.observable("");
-
-//        //this.canEnterPassword = ko.observable(false);
-
-//        //this.loginCommand = Bifrost.commands.Command.create({
-//        //    options: {
-//        //        name: "Login",
-//        //        context: self,
-//        //        properties: {
-//        //            userName: ko.observable(""),
-//        //            password: ko.observable("")
-//        //        },
-//        //        beforeExecute: clearMessages,
-//        //        success: function () {
-//        //            session.getUserIdFor(self.loginCommand.userName()).continueWith(function (userId) {
-//        //                session.setSessionId(userId);
-//        //                History.pushState({}, "", "/home");
-//        //            });
-//        //        },
-//        //        error: function (e) {
-//        //            if (e.validationResults.length) {
-//        //                self.message(e.validationResults[0].errorMesssage);
-//        //            }
-//        //            if (e.commandValidationMessages && e.commandValidationMessages.length) {
-//        //                self.message(e.commandValidationMessages[0]);
-//        //            }
-//        //        }
-//        //    }
-//        //});
-
-////    }
-//);
-//})();
